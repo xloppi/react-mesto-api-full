5e50a2c9676d7eaae458e158ae55dd42860c447b
xloppi <hlopkovalesha@yandex.ru>
2021-06-26 11:44:34 +0300

    bugfix: SameSite none

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -148,7 +148,7 @@ const login = (req, res, next) => {
             .cookie('jwt', token, {
               maxAge: 3600000 * 24 * 7,
               httpOnly: true,
-              sameSite: 'None',
+              SameSite: 'None',
             })
             .end();
         },
