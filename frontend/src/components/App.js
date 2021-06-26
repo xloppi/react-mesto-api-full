1f8ad63d17181ba120a26f7142ccad288e46fa04
xloppi <hlopkovalesha@yandex.ru>
2021-06-26 14:57:56 +0300

    hotfix: like card

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -74,7 +74,7 @@ function App() {
   }
 
   const handleCardLike = (card) => {
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(i => i === currentUser._id);
 
     api.changeLikeCardStatus(card._id, isLiked).then((newCard) => {
         setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,7 +5,7 @@ function Card({card, onCardClick, onCardLike, onCardDelete}) {
 
   const currentUser = React.useContext(CurrentUserContext);
   const isOwn = card.owner === currentUser._id;
-  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const isLiked = card.likes.some(i => i === currentUser._id);
   const cardDeleteButtonClassName = (
     `elements__card-delete-button ${isOwn && 'elements__card-delete-button_visible'}`
   );
